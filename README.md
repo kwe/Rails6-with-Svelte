c0cd09887b29017baa10caf29d08e04af6a67d49
kwe <kwevans@gmail.com>
2020-07-30 13:02:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+Just a simple Rails 6 app with Svelete added via Webpacker.
+
+* ...
---
+++ app/javascript/app.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: #FF3E00;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<p>Welcome from Svelete</p>
